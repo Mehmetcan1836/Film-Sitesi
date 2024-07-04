b6f9112d3b50bba4e9ac8467190e7c41b6092c42
Mehmetcan Çaycı <159884764+Mehmetcan1836@users.noreply.github.com>
2024-07-04 17:18:03 +0300

    updateV2.1

--- javascript.js
+++ javascript.js
@@ -94,36 +94,27 @@ function displayComments() {
 // Initial call to displayComments to display any existing comments
 displayComments();
 
-const arrows = document.querySelectorAll(".arrow");
+const carousel = document.querySelector('.carousel');
+  const posters = document.querySelectorAll('.poster');
+  const leftButton = document.querySelector('.carousel-button.left');
+  const rightButton = document.querySelector('.carousel-button.right');
 
-const movieLists = document.querySelectorAll(".movie-list");
+  let currentPosterIndex = 0;
 
-arrows.forEach((arrow, i) => {
-    const widthRtio = Math.floor(window.innerWidth / 280);
-    let clickCounter = 0;
-    const imageItem = movieLists[i].querySelectorAll("img").length;
-    arrow.addEventListener("click", function () {
-        if (imageItem - (5 + clickCounter) + (5- widthRtio) >= 0) {
-            clickCounter++;
-            movieLists[i].style.transform = `translateX(${
-                movieLists[i].computedStyleMap().get("transform")[0].x.value - 280}px)`;
+  leftButton.addEventListener('click', () => {
+    currentPosterIndex--;
+    if (currentPosterIndex < 0) {
+      currentPosterIndex = posters.length - 1;
+    }
+    carousel.scrollLeft = posters[currentPosterIndex].offsetLeft;
+  });
 
-        } else {
-            movieLists[i].style.transform = "translateX(0)"
-            clickCounter = 0;
-        }
-    });
-});
-
-
-/*dark mode */
-
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title")
-
-ball.addEventListener("click", function(){
-    items.forEach((item) => item.classList.toggle("active"))
-
-})
+  rightButton.addEventListener('click', () => {
+    currentPosterIndex++;
+    if (currentPosterIndex >= posters.length) {
+      currentPosterIndex = 0;
+    }
+    carousel.scrollLeft = posters[currentPosterIndex].offsetLeft;
+  });
 
 
